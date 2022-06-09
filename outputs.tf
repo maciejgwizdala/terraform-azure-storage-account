7fc167c0ca208f29a654449ebdd25cfe2789f8a7
Maciej Gwizdała <57432150+maciejgwizdala@users.noreply.github.com>
2022-06-09 08:34:04 +0200

    Remove sensitive flag from output
    Terraform-runner requires string output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "blob_connection_string" {
-  sensitive = true
-  value     = azurerm_storage_account.this.primary_connection_string
+  value = azurerm_storage_account.this.primary_connection_string
 }
 
 output "web_url" {
